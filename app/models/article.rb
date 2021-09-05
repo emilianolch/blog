d539909f849f50d40bbed87ee60eb2ad19d828b3
Emiliano Lopez <emilianolch@gmail.com>
2021-09-05 00:47:33 -0300

    delete comments when deleting an article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   include Visible
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
