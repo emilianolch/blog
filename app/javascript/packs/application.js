dc3dcd6161e3f7ed047e7f553ce623dcf1a6b718
Emiliano Lopez <emilianolch@gmail.com>
2021-09-05 23:14:43 -0300

    adding bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,19 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Bootstrap 5
+// import 'bootstrap/js/src/alert'  
+// import 'bootstrap/js/src/button'  
+// import 'bootstrap/js/src/carousel'  
+import 'bootstrap/js/src/collapse'  
+// import 'bootstrap/js/src/dropdown'  
+// import 'bootstrap/js/src/modal'  
+// import 'bootstrap/js/src/popover'  
+// import 'bootstrap/js/src/scrollspy'  
+// import 'bootstrap/js/src/tab'  
+// import 'bootstrap/js/src/toast'  
+// import 'bootstrap/js/src/tooltip'  
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
