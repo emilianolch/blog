4e075c0f444f05688e25bca54233ac474846a39f
Emiliano Lopez <emilianolch@gmail.com>
2021-09-08 20:00:31 -0300

    send text messages to telegram from rails console

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,18 +1,30 @@
 # Interface to Telegram Bot API
-require 'httpclient'
+require "httpclient"
 
 class Bot
   API_BASE_URL = "https://api.telegram.org/bot"
+  CHAT_ID = "-536716532"
 
   def initialize
-    id = File.read(Rails.root.join('telegram-bot'))
+    id = File.read(Rails.root.join("telegram-bot"))
     @url = API_BASE_URL + id
+    @clnt = HTTPClient.new
   end
 
   def get_updates
+    client = HTTPClient.new
+
     Thread.new do
-      client = HTTPClient.new
-      puts client.get_content(@url + '/getUpdates')
+      response = client.get_content(@url + "/getUpdates")
+      yield(response)
     end
+  end
+
+  # Send a text message to the default telegram group
+  def send_message(text)
+    @clnt.post(@url + "/sendMessage", {
+      chat_id: CHAT_ID,
+      text: text,
+    })
   end
 end
